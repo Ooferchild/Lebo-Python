edbda99aaf130a53181af6286efa7a51d452bcee
Ooferchild <joshuak194@nycstudents.net>
2022-10-25 12:12:28 -0400

    10-25

---
+++ venv/Lib/Variables/AsciiArt1.py
@@ -0,0 +1,10 @@
+#Josh
+#10-24 -> ASCII Art
+##############
+import pyfiglet
+
+name = input("What is your name? ")
+#print(f"Hello {name}!")
+#mood = input("How are you today?")
+#print(f"I am {mood} too.")
+#print("I am going to print your name in ASCII art.")
